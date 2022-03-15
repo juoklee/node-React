d680f95c9ac474811471d3e3d70c80dc945753ed
이주옥 <juok9802@gmail.com>
2022-03-15 18:04:48 +0900

    Proxy and Concurrently 설치

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -4,7 +4,9 @@ import axios from 'axios';
 function LandingPage() {
 
     useEffect(() => {
+        //get으로 지정된 API에서 정보를 가져온다.
         axios.get('/api/hello')
+        //그 다음(then) 정보를 로그로 보여준다.
         .then(response => { console.log(response)})
 
     }, [])
