2d036b293f92ad0bc46334cde422f6984234a0d4
이주옥 <juok9802@gmail.com>
2022-03-18 00:31:14 +0900

    user_reducer 수정

--- client/src/_reducers/user_reducer.js
+++ client/src/_reducers/user_reducer.js
@@ -7,12 +7,15 @@ import {
 //state = pre, action
 export default function (state = {}, action) {
     switch (action.type) {
+        //로그인
         case LOGIN_USER:
             return {...state, loginSuccess: action.payload }
             break;
+        //회원가입
         case REGISTER_USER:
             return {...state, register: action.payload }
             break;
+        //그 외 현재 state 유지
         default:
             return state;
     }
